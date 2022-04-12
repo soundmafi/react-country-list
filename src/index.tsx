56449d308d6920960af8a738d10c720231a9134f
soundmafi <soundmafi@gmail.com>
2022-04-12 09:15:52 +0300

    fix(index.tsx): delte operator as

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import {createRoot} from 'react-dom/client'
 import App from './App';
 
-const container = document.getElementById('root') as HTMLElement;
+const container = document.getElementById('root');
 
 if (!container) {
     throw new Error("No root element found");
